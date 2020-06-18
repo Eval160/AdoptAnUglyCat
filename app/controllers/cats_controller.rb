ce410aae770c7ec0268cc09506a5f947e7b94b62
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2020-06-18 08:41:50 +0200

    new and create cats

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,5 +1,5 @@
 class CatsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :new, :create]
 
   def index
     @cats = Cat.all
@@ -9,5 +9,25 @@ class CatsController < ApplicationController
     @cats = Cat.all
     @cat = Cat.find(params[:id])
     @purchase = Purchase.new
+  end
+
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      redirect_to cat_path(@cat)
+    else
+      render new_cat_path
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :description, :photo)
   end
 end
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,5 +1,6 @@
 class Cat < ApplicationRecord
   has_many :purchases
+  has_one_attached :photo
   validates :name, presence: true
 
 end
